f73db0f6eaf41725a650d5491b3de3ee993389ce
KenanNagiyev <788lnmp@code.edu.az>
2022-03-03 22:20:17 +0400

    new proekt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int say = 0;
+            for (int i = n; i < m; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    say++;
+                }
+            }
+            Console.WriteLine(say);
+            Console.ReadLine();
+        }
+    }
+    
+}
